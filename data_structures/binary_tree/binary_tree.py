0bd1278780770dbbe712f389531c15f1504fd770
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2022-08-03 00:37:15 -0300

    feat: implement basic binary tree functions

--- data_structures/binary_tree/binary_tree.py
+++ data_structures/binary_tree/binary_tree.py
@@ -8,13 +8,29 @@ class Node:
         self.data = data
 
     def add(self, data: Any) -> None:
-        pass
+        if self.left_node:
+            self.add_right(Node(data))
+        else:
+            self.add_left(Node(data))
 
-    def add_right(self, data: Any) -> None:
-        pass
+    def add_right(self, node) -> None:
+        self.right_node = node
 
-    def add_left(self, data: Any) -> None:
-        pass
+    def add_left(self, node) -> None:
+        self.left_node = node
 
-    def print_tree(self) -> None:
-        pass
+
+def print_tree(node: Node) -> str:
+    if node:
+        return f"< {node.data} {print_tree(node.left_node)} {print_tree(node.right_node)} >"
+    else:
+        return "<>"
+
+
+# Main Code
+
+root = Node(1)
+root.add(2)
+root.add(3)
+
+print(print_tree(root))
