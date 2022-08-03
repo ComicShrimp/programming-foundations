0db554368d8af5d252674e5db86d1ec46fde4921
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2022-08-02 23:08:56 -0300

    feat: add binary tree file

---
+++ binary_tree/binary_tree.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+
+class Node:
+    def __init__(self, data: Any) -> None:
+        self.left_node = None
+        self.right_node = None
+        self.data = data
+
+    def add(self, data: Any) -> None:
+        pass
+
+    def add_right(self, data: Any) -> None:
+        pass
+
+    def add_left(self, data: Any) -> None:
+        pass
+
+    def print_tree(self) -> None:
+        pass
